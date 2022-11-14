45f57c584d677775836127f892e9f23ee3994ebf
[sai rakesh] <chintalasairakesh@gmail.com>
2022-11-14 18:00:30 +0530

    Add project files.

---
+++ Task_parallel_example_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+namespace namespace1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Task t1 = Task.Factory.StartNew(() => dowork(1, 2000)).ContinueWith((prev)=>otherwork(1,2000));
+            Task t2 = Task.Factory.StartNew(() => dowork(2, 2500));
+            Task t3 = Task.Factory.StartNew(() => dowork(3, 1500));
+
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+
+        }
+        static void dowork(int id, int sleep)
+        {
+            Console.WriteLine("Task {0} is beginning..", id);
+            Thread.Sleep(sleep);
+            Console.WriteLine("Task {0} is completed..", id);
+        }
+        static void otherwork(int id,int sleep)
+        {
+            Console.WriteLine("other task {0} has started", id);
+            Thread.Sleep((int)sleep);
+            Console.WriteLine("other task {0} is completed", id);
+        }
+        
+
+    }
+}
